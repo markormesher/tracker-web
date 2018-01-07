658217ef2bf5df37283cc12546faadc0dc030ee7
Mark Ormesher <me@markormesher.co.uk>
2018-01-07 20:06:36 +0000

    Remove log line

--- src/public/global/tooltips.ts
+++ src/public/global/tooltips.ts
@@ -4,7 +4,6 @@ $(() => {
 	$('.block').hover(
 			(event) => {
 				const family = $(event.target).data('family');
-				console.log(family);
 				$('.block[data-family=\'' + family + '\']').addClass('highlight');
 			},
 			() => {
