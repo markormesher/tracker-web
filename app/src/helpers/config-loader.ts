828c04a5c1450423499c5febb31e2e5102236820
Mark Ormesher <me@markormesher.co.uk>
2018-01-21 15:14:18 +0000

    Remove logging

--- app/src/helpers/config-loader.ts
+++ app/src/helpers/config-loader.ts
@@ -4,10 +4,8 @@ const loadedSecrets: { [key: string]: string } = {};
 
 const getSecret: ((key: string) => string) = (key) => {
 	if (loadedSecrets[key] === undefined) {
-		console.log(`Secret from disk: ${key}`);
 		loadedSecrets[key] = fs.readFileSync(`/run/secrets/${key}`).toString().trim();
 	}
-	console.log(`Secret from cache: ${key}`);
 	return loadedSecrets[key];
 };
 
