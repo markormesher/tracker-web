090f6773da03cbf2e7b99b3fd35802d2619426c0
Mark Ormesher <me@markormesher.co.uk>
2018-01-21 11:36:51 +0000

    Longer timeout for Postgres, update .gitignore

--- copy-live-data.sh
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env bash
-
-ssh deploy@chuck "pg_dump -c -d tracker > trackerpgdump.sql"
-scp deploy@chuck:trackerpgdump.sql .
-cat trackerpgdump.sql | docker exec -i tracker-web-postgres psql -U postgres &>/dev/null
-rm trackerpgdump.sql
-ssh deploy@chuck "rm trackerpgdump.sql"
